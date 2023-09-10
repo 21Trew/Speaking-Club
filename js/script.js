fbbd8d4a61fc0019f8b0aeb6351bffd2e4478226
21Trew <21trew@gmail.com>
2023-09-10 21:33:39 +0300

    added autoplay, fixed some css

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     $('.slider-slides').slick({
         infinite: true,
+        autoplay: true,
+        autoplaySpeed: 8000,
+        speed: 1000,
         slidesToShow: 2,
         slidesToScroll: 1,
         dots: true,
